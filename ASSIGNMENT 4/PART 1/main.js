117ce6123ff456d4acdf397d23a7c6ca15ef8e51
jordyfranciss <jordy.francis@dcmail.ca>
2024-03-18 23:53:29 -0400

    Commit 4 of assignment 4 part 1
    Completed the if blocks.

--- ASSIGNMENT 4/PART 1/main.js
+++ ASSIGNMENT 4/PART 1/main.js
@@ -21,15 +21,27 @@ const insertZ = ["spontaneously combusted",
     randomize.addEventListener('click', result);
 
 function result() {
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory
+    .replaceAll(":insertx:",xItem)
+    .replace(":inserty:", yItem)
+    .replace(":insertz:",zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replace("Bob",name);
 
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${ Math.round((94 - 32) *(5/9))} centigrade`;
+    newStory = newStory.replace("300 pounds", weight).replace("94 fahrenheit",temperature);
 
   }
 

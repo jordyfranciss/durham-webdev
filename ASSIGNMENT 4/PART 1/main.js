29799fb7e9f7471847e33146a2726040845d6f88
jordyfranciss <jordy.francis@dcmail.ca>
2024-03-19 00:40:45 -0400

    Commit 5 of assignment 4 part1

--- ASSIGNMENT 4/PART 1/main.js
+++ ASSIGNMENT 4/PART 1/main.js
@@ -41,10 +41,10 @@ function result() {
   if(document.getElementById("uk").checked) {
     const weight = `${Math.round(300 / 14)} stone`;
     const temperature = `${ Math.round((94 - 32) *(5/9))} centigrade`;
-    newStory = newStory.replace("300 pounds", weight).replace("94 fahrenheit",temperature);
+    newStory = newStory.replaceAll("300 pounds", weight).replaceSAll("94 fahrenheit",temperature);
 
   }
 
-  story.textContent = ;
+  story.textContent =newStory;
   story.style.visibility = 'visible';
 }

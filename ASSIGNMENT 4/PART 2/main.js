de43ad8f584fccdaab2df0bd3cc4f374cb1ced0e
jordyfranciss <jordy.francis@dcmail.ca>
2024-03-20 02:45:57 -0400

    COMMIT 4 PART 2 ASSIGNMENT 4

--- ASSIGNMENT 4/PART 2/main.js
+++ ASSIGNMENT 4/PART 2/main.js
@@ -11,13 +11,28 @@ const imageFilenames = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-imageFilenames.forEach(filename =>{
+imageFilenames.forEach((filename,index) => {
 const newImage = document.createElement('img');
-newImage.src = 'images/'+filename;
-newImage.alt = 'Images: ' + filename;
+newImage.setAttribute('src','images/'+ filename);
+newImage.setAttribute ('alt','Images: ' + filename);
+newImage.setAttribute('data-index',index)
 thumbBar.appendChild(newImage);
 
 });
 
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click',function (){
+   const classList = btn.getAttribute('class');
+   if (classList ==='dark'){
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+   }else{
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+
+   }
+});

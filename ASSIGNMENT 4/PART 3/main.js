75551a5f4ef1035705258bf07ae2934ccdd7784d
jordyfranciss <jordy.francis@dcmail.ca>
2024-03-21 23:47:45 -0400

    COMMIT 4 OF ASSIGNMENT PART4.

--- ASSIGNMENT 4/PART 3/main.js
+++ ASSIGNMENT 4/PART 3/main.js
@@ -18,7 +18,7 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-class ball {
+class Ball {
   constructor(x, y, velX, velY, color, size){
     this.x = x;
     this.y = y;
@@ -54,12 +54,64 @@ class ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  collisionDetect() {
+    for (const ball of balls){
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size){
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
   
 }
-const testball = new ball(50,100,4,4,"blue",10);
-  testBall.x;
-  testBall.size;
-  testBall.color;
-  testBall.draw();
+
+const balls = [];
+
+
+while (balls.length < 25) {
+  
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+
+  );
+ 
+  
+  
+  balls.push(ball);
+}
+
+
+
+
+  function loop() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < balls.length; i++) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+
+  }
+  
+  loop();
+
+
+
 
   

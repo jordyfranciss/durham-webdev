bf9e783b3088c779726d7ab2d22974162d07b5c4
jordyfranciss <jordy.francis@dcmail.ca>
2024-03-21 21:47:26 -0400

    COMMIT 3 OF ASSIGNMENT 4 PART 3

--- ASSIGNMENT 4/PART 3/main.js
+++ ASSIGNMENT 4/PART 3/main.js
@@ -6,6 +6,7 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+
 // function to generate random number
 
 function random(min, max) {
@@ -17,21 +18,48 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-class Ball {
-    constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
-    }
-    draw() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-      ctx.fill();
-    }
-    
+class ball {
+  constructor(x, y, velX, velY, color, size){
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+    ctx.fill();
+  }
+
+  update(){
+    if ((this.x + this.size) >= width) {
+
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+
+    if((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  
+}
+const testball = new ball(50,100,4,4,"blue",10);
+  testBall.x;
+  testBall.size;
+  testBall.color;
+  testBall.draw();
+
   
